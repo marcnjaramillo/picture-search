b908c69983741badf762f2642fb4fcac394c09f3
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-12-22 19:31:19 -0800

    Add event handler for user input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,24 @@
 import React, { Component  } from 'react';
 
 class SearchBar extends Component {
-    
-    render() {
-       return (
-       <div className="ui segment">
-           <form className="ui form">
-            <div className="field">
-                <label>Image Search</label>
-               <input type="text" />
-            </div>
-           </form>
-       </div>
-       );
-    }
+
+  state = { term: '' };
+  
+  render() {
+      return (
+      <div className="ui segment">
+          <form className="ui form">
+          <div className="field">
+              <label>Image Search</label>
+              <input type="text" 
+                  value={this.state.term} 
+                  onChange={(e) => this.setState({ term: e.target.value })}
+              />
+          </div>
+          </form>
+      </div>
+      );
+  }
 };
 
 export default SearchBar;
