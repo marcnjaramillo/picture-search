ec5ab9bbd47b8f4777bef80961b15ffd469c8e09
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-12-22 17:59:56 -0800

    Create and hook up SearchBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 const App = () => {
     return (
-        <div>App</div>
+        <div className="ui container" style={{ marginTop: '10px'}}>
+            <SearchBar />
+        </div>
     )
 }
 
