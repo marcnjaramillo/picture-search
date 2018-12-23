43497eb3bd1bffbcd3e8d9113a1f142af6111d6e
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-12-22 21:16:25 -0800

    Add submit handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,11 +3,17 @@ import React, { Component  } from 'react';
 class SearchBar extends Component {
 
   state = { term: '' };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  }
   
   render() {
       return (
       <div className="ui segment">
-          <form className="ui form">
+          <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
               <label>Image Search</label>
               <input type="text" 
